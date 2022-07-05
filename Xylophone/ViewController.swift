03fd7f3c18a0ed5dac93e4d5143195338c618dfb
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-07-06 00:25:37 +0300

    Создал кнопки Добавил иконки и звуки Запрограммировал логику и обработку звука

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var player: AVAudioPlayer?
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let text = sender.titleLabel?.text else { return }
+        print(text)
+        playSound(forResource: text)
     }
-
-
+    
+// Method wich do play wav source
+    func playSound(forResource: String) -> Void {
+        guard let url = Bundle.main.url(
+            forResource: forResource,
+            withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(
+                .playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(
+                contentsOf: url,
+                fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            player.play()
+            
+            } catch let error { print(error.localizedDescription) }
+    }
 }
 
